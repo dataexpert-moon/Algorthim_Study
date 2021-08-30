4e3be5a5abe27c18398d91fa091ecc34d4c17ea2
RangIe <81904802+dataexpert-moon@users.noreply.github.com>
2021-08-30 21:57:21 +0900

    그리디

--- 그리디/모험가 길드.py
+++ 그리디/모험가 길드.py
@@ -1,5 +1,22 @@
 # 모험가 길드
+# <답안>
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
 
+result = 0 # 총 그룹의 수
+count = 0 # 현재 그룹에 포함된 모험가의 수
+
+for i in data: # 공포돌를 낮은 것부터 하나씩 확인하며
+  count += 1 # 현재 그룹에 해당 모험가를 포함시키기
+  if count >= i: # 현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
+    result += 1
+    count = 0
+print(result)
+
+
+'''
+# <내풀이>
 n = int(input())
 x = list(map(int, input().split()))
 group = 0
@@ -18,21 +35,4 @@ while True:
     break
 
 print(group) # 모험가 그룹 수 출력
-
-
-'''
-n = int(input())
-data = list(map(int, input().split()))
-data.sort()
-
-result = 0 # 총 그룹의 수
-count = 0 # 현재 그룹에 포함된 모험가의 수
-
-for i in data: # 공포돌를 낮은 것부터 하나씩 확인하며
-  count += 1 # 현재 그룹에 해당 모험가를 포함시키기
-  if count >= i: # 현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
-    result += 1
-    count = 0
-
-print(result) 
 '''
